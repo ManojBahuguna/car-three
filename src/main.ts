8fe92896a24a134813fa7a4e738113812f40c112
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2023-01-20 13:40:45 +0530

    setup roads, cars and lighting

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,69 @@
 import { setup } from "./setup";
 import { createCar } from "./car";
+import { BoxGeometry, Mesh, MeshStandardMaterial } from "three";
 
-const [carToFront, moveForward] = createCar();
-carToFront.position.x = -15;
-carToFront.position.z = 60;
+const forwardRoadX = -25;
+const backwardRoadX = 20;
 
-const [carToRight, moveRight] = createCar(14,10,10);
-carToRight.rotation.set(0, -Math.PI / 3, 0);
-carToRight.position.z = 0;
+const [carForward1, moveCarF1] = createCar(22, 14, 10);
+carForward1.position.x = forwardRoadX - 8;
+carForward1.position.z = 300;
+
+const [carForward2, moveCarF2] = createCar(20, 13, 12);
+carForward2.position.x = forwardRoadX + 10;
+carForward2.position.z = 300;
+
+const [carBackward1, moveCarB1] = createCar(18, 10, 10);
+carBackward1.rotation.y = -Math.PI;
+carBackward1.position.x = backwardRoadX - 10;
+carBackward1.position.z = -400;
+
+const [carBackward2, moveCarB2] = createCar(21, 12, 12);
+carBackward2.rotation.y = -Math.PI;
+carBackward2.position.x = backwardRoadX + 10;
+carBackward2.position.z = -400;
 
 const { scene } = setup(() => {
-  moveForward(0.3);
-  moveRight(0.1);
-  // carToRight.position.x += 0.06;
-
-  // carToFront.position.z -= 1;
-
-  // if (carToFront.position.z < -1000) {
-  //   carToRight.position.x = 0;
-  //   carToFront.position.z = 0;
-  // }
+  moveCarF1(0.5, 600);
+  moveCarF2(0.8, 600);
+  moveCarB1(1.1, 600);
+  moveCarB2(0.6, 600);
 });
 
-scene.add(carToRight);
-scene.add(carToFront);
+scene.add(carForward1);
+scene.add(carForward2);
+scene.add(carBackward1);
+scene.add(carBackward2);
+
+// ROADS
+const forwardRoad = new Mesh(
+  new BoxGeometry(40, 2, 2000),
+  new MeshStandardMaterial({ color: 0x222 })
+);
+forwardRoad.position.y = -2;
+forwardRoad.position.x = forwardRoadX;
+
+const backwardRoad = new Mesh(
+  new BoxGeometry(40, 2, 2000),
+  new MeshStandardMaterial({ color: 0x222 })
+);
+backwardRoad.position.y = -2;
+backwardRoad.position.x = backwardRoadX;
+
+function getRoadLine(z: number) {
+  const line = new Mesh(
+    new BoxGeometry(1, 2.1, 10),
+    new MeshStandardMaterial({ color: 0xffffaa })
+  );
+  line.position.z = z;
+  return line;
+}
+
+// lines on road
+for (let i = 50; i > -50; i--) {
+  forwardRoad.add(getRoadLine(i * 20));
+  backwardRoad.add(getRoadLine(i * 20));
+}
+
+scene.add(forwardRoad);
+scene.add(backwardRoad);
--- src/setup.ts
+++ src/setup.ts
@@ -1,17 +1,25 @@
-import * as THREE from "three";
+import {
+  AmbientLight,
+  Color,
+  DirectionalLight,
+  Fog,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
 
 export function setup(render: (args: ReturnType<typeof setup>) => void) {
   const container = document.querySelector<HTMLDivElement>("#app")!;
 
   // Setup renderer and add to DOM
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  const renderer = new WebGLRenderer({ antialias: true });
   container.appendChild(renderer.domElement);
 
   // Setup Camera
-  const camera = new THREE.PerspectiveCamera();
-  camera.position.set(10, 64, 60);
-  camera.rotation.set(-0.7, 0, 0);
-  // camera.position.set(0, 0, 16);
+  const camera = new PerspectiveCamera();
+  camera.position.set(20, 80, 200);
+  camera.rotation.set(-0.7, 0.2, 0.3);
+  // camera.position.set(-20, 0, 100);
 
   // Setup size
   function resize() {
@@ -21,18 +29,73 @@ export function setup(render: (args: ReturnType<typeof setup>) => void) {
 
     renderer.setSize(width, height);
     camera.aspect = aspect;
+    camera.updateProjectionMatrix();
   }
   resize();
   new ResizeObserver(resize).observe(container);
 
   // Setup scene
-  const scene = new THREE.Scene();
+  const scene = new Scene();
+  scene.background = new Color(0x111122);
+  scene.fog = new Fog(0x332222, -100, 400);
+
+  // Directional light
+  const directionalLight = new DirectionalLight(0xffffff, 0.2);
+  scene.add(directionalLight);
+
+  const ambientLight = new AmbientLight(0xffffff, 0.2);
+  scene.add(ambientLight);
+
+  const cameraEffectSpeed = 0.5;
+  const minCamY = 10;
+  const maxCamPos = 20;
+  let xSpeed = -0.02 * cameraEffectSpeed;
+  let ySpeed = 0.01 * cameraEffectSpeed;
+  let zSpeed = 0.05 * cameraEffectSpeed;
+
+  const minRotationX = -0.1;
+  const maxRotation = 0.4;
+  let rotateXSpeed = 0.0001 * cameraEffectSpeed;
+  let rotateYSpeed = -0.0002 * cameraEffectSpeed;
 
   // Start animation
   function animate() {
     window.requestAnimationFrame(animate);
 
     render({ renderer, scene, camera });
+
+    // camera movement effect
+    scene.position.x += xSpeed;
+    scene.position.y += ySpeed;
+    scene.position.z += zSpeed;
+
+    if (scene.position.x > maxCamPos || scene.position.x < -maxCamPos) {
+      xSpeed = -xSpeed;
+    }
+    if (
+      (ySpeed > 0 && scene.position.y > maxCamPos) ||
+      (ySpeed < 0 && scene.position.y < minCamY)
+    ) {
+      ySpeed = -ySpeed;
+    }
+    if (scene.position.z > maxCamPos || scene.position.z < -maxCamPos) {
+      zSpeed = -zSpeed;
+    }
+
+    // camera rotation effect
+
+    scene.rotation.x += rotateXSpeed;
+    scene.rotation.y += rotateYSpeed;
+
+    if (
+      (rotateXSpeed > 0 && scene.rotation.x > maxRotation) ||
+      (rotateXSpeed < 0 && scene.rotation.x < minRotationX)
+    ) {
+      rotateXSpeed = -rotateXSpeed;
+    }
+    if (scene.rotation.y > maxRotation || scene.rotation.y < -maxRotation) {
+      rotateYSpeed = -rotateYSpeed;
+    }
 
     renderer.render(scene, camera);
   }
